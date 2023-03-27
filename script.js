2ac549dec205c840f7b6ec60107e15f81c17324d
JaironRodrigo <jaironcreed@gmail.com>
2023-03-27 11:16:22 -0300

    Criação dos arquivos html, css e javascript do projeto

---
+++ script.js
@@ -0,0 +1,27 @@
+const boardRegions = document.querySelectorAll("#gameBoard span");
+let vBoard = [];
+let turnPlayer = "";
+
+function updateTitle() {
+  const playerInput = document.getElementById("turnPlayer");
+  document.getElementById("turnPlayer").innerText = playerInput.value;
+}
+
+function initializeGame() {
+  vBoard = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  turnPlayer = "player1";
+  document.querySelector("h2").innerHTML =
+    'Vez de: <span id="turnPlayer"></span>';
+  updateTitle();
+
+  boardRegions.forEach(function (element) {
+    element.classList.remove("win");
+    element.innerText = "";
+    element.classList.add("cursor-pointer");
+    element.addEventListener("click", handleBoardCLick);
+  });
+}
